bba3ec8844423606b6a28430a31f93eabfabbb4c
Arman shekh <mdarmanshekh4@gmail.com>
2024-07-31 02:00:29 +0530

    jdvln

--- backend/routes/employeeRoute.js
+++ backend/routes/employeeRoute.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/'); 
+    cb(null, '../uploads/'); 
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname)); 
